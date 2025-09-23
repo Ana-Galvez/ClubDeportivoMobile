aa7c0e1829d1b6166ea4908349ad9eb04e67e45c
amg1688 <anamariagalvez1688@gmail.com>
2025-09-23 18:02:49 -0300

    feat: diseño terminado

--- app/src/main/java/com/example/clubdeportivomovile/ReciboPagoSocio.kt
+++ app/src/main/java/com/example/clubdeportivomovile/ReciboPagoSocio.kt
@@ -1,6 +1,8 @@
 package com.example.clubdeportivomovile
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,18 @@ class ReciboPagoSocio : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_recibo_pago_socio)
 
+        val spinnerPago: Spinner = findViewById(R.id.spinner_pago)
+
+        // Crea un ArrayAdapter usando el string-array y un layout simple
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.opciones_pago,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Especifica el layout para usar cuando la lista de opciones aparece
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Aplica el adaptador al spinner
+            spinnerPago.adapter = adapter
+        }
     }
 }
