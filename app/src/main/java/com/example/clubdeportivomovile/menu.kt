cf51029e64b9fbf45f6a7f32c23ccb77b249f555
gissestephy <gisela.s.colmeiro@gmail.com>
2025-10-02 14:33:24 -0300

    Se solucionaron algunos bugs en menu y sitios

--- app/src/main/java/com/example/clubdeportivomovile/menu.kt
+++ app/src/main/java/com/example/clubdeportivomovile/menu.kt
@@ -1,65 +1,20 @@
 package com.example.clubdeportivomovile
 
-import android.app.Dialog
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class menu : AppCompatActivity() {
+class apto_fisico : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu_container)) { v, insets ->
+        setContentView(R.layout.activity_apto_fisico)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Referencias a los TextView
-        /*
-        val inicio = findViewById<TextView>(R.id.txtInicio)
-        val appFisico = findViewById<TextView>(R.id.txtAppFisico)
-        val ayuda = findViewById<TextView>(R.id.txtAyuda)
-        val configuracion = findViewById<TextView>(R.id.txtConfiguracion)
-
-*/
-        // Todos abren Sitio en construcción
-        val intentSitio = Intent(this, sitioconstruccion::class.java)
-/*
-        inicio.setOnClickListener { startActivity(intentSitio) }
-        appFisico.setOnClickListener { startActivity(intentSitio) }
-        ayuda.setOnClickListener { startActivity(intentSitio) }
-        configuracion.setOnClickListener { startActivity(intentSitio) }
-*/
-    }
-
-    private fun mostrarDialogoSalir() {
-        val dialog = Dialog(this)
-        dialog.setContentView(R.layout.activity_cerrarsesion)
-        dialog.setCancelable(false)
-
-        val btnSi = dialog.findViewById<android.widget.Button>(R.id.btn_si)
-        val btnNo = dialog.findViewById<android.widget.Button>(R.id.btn_no)
-
-        btnSi.setOnClickListener {
-            dialog.dismiss()
-            finishAffinity()
-
-            // Para que vaya a la pantalla de Login después de cerrar,
-            // val intentLogin = Intent(this, LoginActivity::class.java)
-            // startActivity(intentLogin)
-        }
-
-        btnNo.setOnClickListener {
-            dialog.dismiss()
-        }
-
-        dialog.show()
     }
 }
