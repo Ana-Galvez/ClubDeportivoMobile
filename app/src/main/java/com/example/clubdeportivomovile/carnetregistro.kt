c07eeb9db80c4203484dd1afddfa64e4114db8de
gissestephy <gisela.s.colmeiro@gmail.com>
2025-10-03 15:10:39 -0300

    Integración de la rama pantallas_iniciales a master

---
+++ app/src/main/java/com/example/clubdeportivomovile/carnetregistro.kt
@@ -0,0 +1,20 @@
+package com.example.clubdeportivomovile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class carnetregistro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_carnetregistro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
