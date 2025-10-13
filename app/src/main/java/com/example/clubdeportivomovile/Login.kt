0919e953c4122fdec04aeaa8a79e0ead03deb3b9
gissestephy <gisela.s.colmeiro@gmail.com>
2025-10-13 11:45:48 -0300

    Se agrego la validación en el Login

--- app/src/main/java/com/example/clubdeportivomovile/Login.kt
+++ app/src/main/java/com/example/clubdeportivomovile/Login.kt
@@ -7,16 +7,21 @@ import android.text.method.PasswordTransformationMethod
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.CheckBox
 
 class Login : AppCompatActivity() {
 
+    private lateinit var usuarioEditText: EditText
     private lateinit var contraseñaEditText: EditText
     private lateinit var toggleImageView: ImageView
     private var isPasswordVisible = false
+
+    private lateinit var checkBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +29,34 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val btnLogin = findViewById<Button>(R.id.btnLogin)
+        usuarioEditText = findViewById(R.id.Usuario)
         contraseñaEditText = findViewById(R.id.Contraseña)
         toggleImageView=findViewById(R.id.ivToggle)
+        checkBox = findViewById(R.id.checkBox)
         btnLogin.setOnClickListener {
-            val intent = Intent (this, Home::class.java)
-            startActivity(intent)
+            val usuario = usuarioEditText.text.toString()
+            val pass = contraseñaEditText.text.toString()
+
+            if (usuario.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(this, "Complete todos los campos", Toast.LENGTH_SHORT).show()
+            }
+            else if (!checkBox.isChecked) {
+                Toast.makeText(this, "Debe aceptar los términos y condiciones", Toast.LENGTH_SHORT).show()
+            }
+            else if (usuario != "admin" && pass == "1234") {
+                Toast.makeText(this, "Ingresa un nombre de usuario válido", Toast.LENGTH_SHORT).show()
+            }
+            else if (usuario == "admin" && pass != "1234") {
+                Toast.makeText(this, "Contraseña incorrecta. Intenta nuevamente", Toast.LENGTH_SHORT).show()
+            }
+            else if (usuario != "admin" && pass != "1234"){
+                Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+            }
+            else if (usuario == "admin" && pass == "1234") {
+                Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
         }
 
         toggleImageView.setOnClickListener {
