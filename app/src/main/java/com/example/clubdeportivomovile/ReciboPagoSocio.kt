cc468f8f1c0aadbd3abadf4381cc89a5536daed3
amg1688 <anamariagalvez1688@gmail.com>
2025-10-22 20:11:45 -0300

    feat: validación en curso registro pago socio

--- app/src/main/java/com/example/clubdeportivomovile/ReciboPagoSocio.kt
+++ app/src/main/java/com/example/clubdeportivomovile/ReciboPagoSocio.kt
@@ -2,12 +2,16 @@ package com.example.clubdeportivomovile
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,7 +27,146 @@ class ReciboPagoSocio : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recibo_pago_socio)
 
+        // Configurar header con botón atrás + hamburguesa
+        setupHeader(drawerLayout)
+        setupDrawerMenu(R.id.drawerLayout) ///********** agregue para fc del menu ---va el id como parametro
+        setupBottomBar("pagos")  //activo botones barra
+
+        //clientes
+        data class Cliente(
+            val id: Int,
+            val nombre: String,
+            val apellido: String,
+            val socio: Boolean,
+            val telefono: String,
+            val ins: String,
+            val direccion:String
+        )
+
+        val clientes = listOf(
+            Cliente(1, "María", "Golden", false, "09012345", "02/07/2024", "Calle Falsa 01"),
+            Cliente(1, "María", "Silver", true, "523112345", "30/10/2024", "Callejón New"),
+            Cliente(2, "Juan", "Chavo", true, "01112345", "15/04/2025", "Calle NoHay 10"),
+            Cliente(3, "Lorena", "Sim", false, "04662345", "22/12/2024", "Calle S/N"),
+            Cliente(3, "Armando", "Perez Gomez", false, "222662345", "13/12/2024", "Calle Nueva")
+        )
+
+        val clienteEditText: AutoCompleteTextView= findViewById(R.id.cliente)
+
+        val nombresClientes = clientes.map { "${it.nombre} ${it.apellido}" }
+
+        val adapterClientes = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            nombresClientes
+        )
+
+        // Para mostrar sugerencia de busqueda de nombres
+        clienteEditText.setAdapter(adapterClientes)
+
+
+        // Cuota pendiente
+        data class Cuota(
+            val id: Int,
+            val mesAnio: String,
+            val monto: Int
+        )
+
+        val cuotas = listOf(
+            Cuota(1, "Septiembre 2025", 35000),
+            Cuota(2, "Julio 2025", 35000),
+            Cuota(3, "Agosto 2025", 35000)
+        )
+
+        val spinnerCuota: Spinner = findViewById(R.id.spinner_cuota_pendiente)
+        val montoEditText: EditText = findViewById(R.id.monto)
+
+        val nombresCuotas = mutableListOf("Seleccionar...")
+        nombresCuotas.addAll(cuotas.map { it.mesAnio })
+
+        val adapter = object : ArrayAdapter<String>(
+            this,
+            R.layout.spinner_item_custom,
+            nombresCuotas
+        ) {
+            override fun isEnabled(position: Int): Boolean {
+                // placeholder
+                return position != 0
+            }
+        }
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCuota.adapter = adapter
+
+        spinnerCuota.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0) {
+                    montoEditText.text.clear()
+                    return
+                }
+
+                val cuotaSeleccionada = cuotas[position - 1] // -1 por el placeholder
+                montoEditText.setText(cuotaSeleccionada.monto.toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        spinnerCuota.setSelection(0)
+
+        adapter.setDropDownViewResource(R.layout.spinner_item_custom)
+        spinnerCuota.adapter = adapter
+
+        spinnerCuota.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0) {
+                    montoEditText.text.clear()
+                    return
+                }
+
+                val cuotaSeleccionada = cuotas[position - 1] // -1 por el placeholder
+                montoEditText.setText(cuotaSeleccionada.monto.toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        // Mostrar el placeholder
+        spinnerCuota.setSelection(0)
+
+        // Drawer
+        drawerLayout = findViewById(R.id.drawerLayout)
+        setupHeader(drawerLayout)
+
+
+        // Forma de pago
+        data class FormaPago(
+            val id: Int,
+            val medioPago: String,
+        )
+
+        val formasPagos = listOf(
+            FormaPago(1, "Efectivo"),
+            FormaPago(2, "Tarjeta de crédito"),
+        )
+
         val spinnerPago: Spinner = findViewById(R.id.spinner_pago)
+
+        val nombresPago = mutableListOf("Seleccionar...")
+        nombresPago.addAll(formasPagos.map { it.medioPago })
+
+/*        val spinnerPago: Spinner = findViewById(R.id.spinner_pago)
 
         // Crea un ArrayAdapter usando el string-array y un layout simple
         ArrayAdapter.createFromResource(
@@ -37,19 +180,97 @@ class ReciboPagoSocio : BaseActivity() {
             spinnerPago.adapter = adapter
         }
 
-        drawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)*/
 
-        // Configurar header con botón atrás + hamburguesa
-        setupHeader(drawerLayout)
-        setupDrawerMenu(R.id.drawerLayout) ///********** agregue para fc del menu ---va el id como parametro
-        setupBottomBar("pagos")  //activo botones barra
+        //Validaciones del formulario
+        fun validarFormulario(
+            spinnerCuota: Spinner,
+            clienteEditText: EditText,
+            clientes: List<Cliente>
+        ): Boolean {
+            val nombreIngresado = clienteEditText.text.toString().trim()
 
+            val cuotaSeleccionada = spinnerCuota.selectedItemPosition != 0
+            val clienteIngresado = nombreIngresado.isNotEmpty()
+
+            // campos vacíos
+            if (!clienteIngresado && !cuotaSeleccionada) {
+                Toast.makeText(
+                    this,
+                    "Campos incompletos. Complete cliente y cuota.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            } else if (!clienteIngresado) {
+                Toast.makeText(this, "Debe ingresar el nombre del cliente.", Toast.LENGTH_SHORT)
+                    .show()
+                return false
+            } else if (!cuotaSeleccionada) {
+                Toast.makeText(this, "Debe seleccionar una cuota.", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            // Validar que el nombre tenga solo letras y espacios
+            val regexNombre = Regex("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+\$")
+            if (!regexNombre.matches(nombreIngresado)) {
+                Toast.makeText(
+                    this,
+                    "El nombre del cliente contiene caracteres inválidos.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            }
+
+            // Buscar cliente en la base de datos
+            val clienteEncontrado = clientes.find {
+                "${it.nombre} ${it.apellido}".equals(nombreIngresado, ignoreCase = true)
+            }
+
+            if (clienteEncontrado == null) {
+                Toast.makeText(this, "El cliente no existe.", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            // Verificar si es no socio
+            if (!clienteEncontrado.socio) {
+                Toast.makeText(
+                    this,
+                    "El cliente es NO socio no se puede registrar el pago.",
+                    Toast.LENGTH_LONG
+                ).show()
+                return false
+            }
+            return true
+        }
+
+        //Botones
         val botonAceptar: Button = findViewById(R.id.btnAceptarSocio)
 
         botonAceptar.setOnClickListener {
-            val intent = Intent(this, ReciboSocioActivity::class.java)
-            startActivity(intent)
+            if (validarFormulario(spinnerCuota, clienteEditText, clientes = clientes)) {
+                val cuotaSeleccionada = spinnerCuota.selectedItem.toString()
+                val clienteNombre = clienteEditText.text.toString().trim()
+                val monto = montoEditText.text.toString().trim()
+                //Para pasar los datos del cliente socio encontrado
+                val clienteEncontrado = clientes.find {
+                    "${it.nombre} ${it.apellido}".equals(clienteNombre, ignoreCase = true)
+                }!!
+                //Paso la info al comprobante
+                val intent = Intent(this, ReciboSocioActivity::class.java).apply {
+                    putExtra("nombreCliente", clienteNombre)
+                    putExtra("cuota", cuotaSeleccionada)
+                    putExtra("monto", monto)
+                    putExtra("formaPago",formaPago)
+                    putExtra("telefono", clienteEncontrado.telefono)
+                    putExtra("ins", clienteEncontrado.ins)
+                    putExtra("direccion", clienteEncontrado.direccion)
+                }
+
+                startActivity(intent)
+            }
         }
+
+
 
         val botonLimpiar: Button = findViewById(R.id.btnLimpiarSocio)
         val etNombre: EditText = findViewById(R.id.cliente)
--- app/src/main/java/com/example/clubdeportivomovile/ReciboSocioActivity.kt
+++ app/src/main/java/com/example/clubdeportivomovile/ReciboSocioActivity.kt
@@ -1,6 +1,7 @@
 package com.example.clubdeportivomovile
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,5 +21,30 @@ class ReciboSocioActivity : BaseActivity() {
         setupDrawerMenu(R.id.drawerLayout) // menu ---va el id como parametro
 
         setupBottomBar("")//barra
+
+        // Datos del formulario
+        val nombreCliente = intent.getStringExtra("nombreCliente")
+        val cuota = intent.getStringExtra("cuota")
+        val monto = intent.getStringExtra("monto")
+        val formaPago = intent.getStringExtra("formaPago")
+        val direccion = intent.getStringExtra("direccion")
+        val telefono = intent.getStringExtra("telefono")
+        val inscrip = intent.getStringExtra("ins")
+
+        val tvNombreSocio = findViewById<TextView>(R.id.tvNombreSocio)
+        val tvCuota = findViewById<TextView>(R.id.tvCuotaSocio)
+        val tvMonto = findViewById<TextView>(R.id.tvMonto)
+        val tvFormaPago = findViewById<TextView>(R.id.tvFormaPago)
+        val tvDireccion = findViewById<TextView>(R.id.tvDireccion)
+        val tvTelefono = findViewById<TextView>(R.id.tvTelefono)
+        val tvInsc = findViewById<TextView>(R.id.tvFechaInsc)
+
+        tvNombreSocio.text = nombreCliente
+        tvCuota.text = cuota
+        tvMonto.text = "$$monto"
+        tvFormaPago.text = formaPago
+        tvDireccion.text = direccion
+        tvTelefono.text = telefono
+        tvInsc.text = inscrip
     }
 }
