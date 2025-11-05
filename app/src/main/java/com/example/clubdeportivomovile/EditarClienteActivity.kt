e915a6d159a27a1ccae6b671e83a663696b71473
LorenaCoheneBaez <loreley.cb15@gmail.com>
2025-11-04 22:27:14 -0300

    Fix edit to name and lastname to carnet

--- app/src/main/java/com/example/clubdeportivomovile/EditarClienteActivity.kt
+++ app/src/main/java/com/example/clubdeportivomovile/EditarClienteActivity.kt
@@ -1,15 +1,18 @@
 package com.example.clubdeportivomovile
 
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.clubdeportivomovile.limpiarFormulario
+import java.util.Calendar
 
 class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi tiene la fc de la barra
     private lateinit var drawerLayout: DrawerLayout
@@ -22,6 +25,27 @@ class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi t
         setupHeader(drawerLayout)
         setupDrawerMenu(R.id.drawerLayout) /// menu ---va el id como parametro
         setupBottomBar("")  //barra, cadena vacia para q no resalte ningun boton de la barra
+        //Editar fecha de nacimiento
+        val rowFechaNac = findViewById<LinearLayout>(R.id.rowFechaNac)
+        val tvFechaNac: TextView = findViewById(R.id.tvFechaNac)
+
+        rowFechaNac.setOnClickListener {
+            val calendario = Calendar.getInstance()
+            val anio = calendario.get(Calendar.YEAR)
+            val mes = calendario.get(Calendar.MONTH)
+            val dia = calendario.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(
+                this,
+                { _, year, month, dayOfMonth ->
+                    // Cuando el usuario selecciona la fecha:
+                    val fechaSeleccionada = "%02d/%02d/%04d".format(dayOfMonth, month + 1, year)
+                    tvFechaNac.text = fechaSeleccionada
+                },
+                anio, mes, dia
+            )
+            datePicker.show()
+        }
 
         //Botones Limpiar
         val botonLimpiar: Button = findViewById(R.id.btnLimpiar)
@@ -36,15 +60,13 @@ class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi t
         }
 
         //Formulario
-        //REcibo datos del cliente
+        //Recibo datos del cliente
         val id = intent.getIntExtra("id", -1)
         //Formulario editar necesita nombre y apellido separado
         val nombre = intent.getStringExtra("nombre") ?: ""
         val apellido = intent.getStringExtra("apellido") ?: ""
-        //Carnet necesita nombre completo
-        val nombreCompleto = intent.getStringExtra("nombreCompleto") ?: ""
         val dni = intent.getIntExtra("dni", 0)
-        val fechaNacimiento = intent.getStringExtra("fechaNacimiento")
+        val fechaNacimientoEditada = intent.getStringExtra("fechaNacimiento")
         val direccion = intent.getStringExtra("direccion")
         val telefono = intent.getStringExtra("telefono")
         val genero = intent.getStringExtra("genero")
@@ -53,7 +75,6 @@ class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi t
         //Agrego los datos al formulario
         val etApellido = findViewById<EditText>(R.id.etApellido)
         val etDni = findViewById<EditText>(R.id.etDni)
-        val etFechaNac = findViewById<TextView>(R.id.tvFechaNac)
         val etDireccion = findViewById<EditText>(R.id.etDireccion)
         val etTelefono = findViewById<EditText>(R.id.etTelefono)
         //Genero (radio buttons)
@@ -68,7 +89,7 @@ class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi t
         etNombre.setText(nombre)
         etApellido.setText(apellido)
         etDni.setText(dni.toString())
-        etFechaNac.text=fechaNacimiento
+        tvFechaNac.text=fechaNacimientoEditada
         etDireccion.setText(direccion)
         etTelefono.setText(telefono)
         //Genero (radio buttons)
@@ -90,10 +111,13 @@ class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi t
         botonAceptar.setOnClickListener {
             val nombre = etNombre.text.toString()
             val apellido = etApellido.text.toString()
+            //Envio el nombre completo editado si se modifico
+            val nombreCompletoEditado = "$nombre $apellido"
             val dni = etDni.text.toString().toIntOrNull() ?: 0
-            val fechaInscripcion = etFechaNac.text.toString()
             val direccion = etDireccion.text.toString()
             val telefono = etTelefono.text.toString()
+            //Este dato no se edita
+            val fechaInscripcion = fechaInscripcion
 
             // Género
             val genero = when {
@@ -107,8 +131,8 @@ class EditarClienteActivity : BaseActivity() { //cambiamos de quien hereda asi t
 
             // Pasar los datos al carnet
             val intent = Intent(this, CarnetActivity::class.java)
-            intent.putExtra("nombreCompleto", nombreCompleto)
-            intent.putExtra("id", id) // el id del cliente
+            intent.putExtra("nombreCompleto", nombreCompletoEditado)
+            intent.putExtra("id", id)
             intent.putExtra("dni", dni)
             intent.putExtra("fechaInscripcion", fechaInscripcion)
             intent.putExtra("direccion", direccion)
