63810f52e5295cf927df9d95730a336b9e418119
anamarigalvez1688@gmail.com <amg1688>
2025-11-10 20:59:38 -0300

    feat: agregado a DBHelper tablas (socios, nosocios,cuotas,pago_actividad)

--- app/src/main/java/com/example/clubdeportivomovile/data/DBHelper.kt
+++ app/src/main/java/com/example/clubdeportivomovile/data/DBHelper.kt
@@ -47,6 +47,49 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "SportifyClub.db", 
                     "Activo INTEGER NOT NULL," +
                     "FOREIGN KEY (RolUsu) REFERENCES roles(RolUsu))"
         )
+
+        db.execSQL(
+            "CREATE TABLE socios(" +
+                    "idCliente INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "FechaAltaSocio TEXT NOT NULL, " +
+                    "FOREIGN KEY (idCliente) REFERENCES clientes(id) ON DELETE CASCADE)"
+        )
+
+        db.execSQL(
+            "CREATE TABLE nosocios(" +
+                    "idCliente INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "FechaAltaNoSocio TEXT NOT NULL, " +
+                    "FOREIGN KEY (idCliente) REFERENCES clientes(id) ON DELETE CASCADE)"
+        )
+
+        db.execSQL(
+            "CREATE TABLE cuotas(" +
+                    "IdCuota INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "id_cliente INTEGER NOT NULL, " +
+                    "Monto REAL NOT NULL, " +
+                    "ModoPago TEXT CHECK (ModoPago IN ('Efectivo','Tarjeta','Transferencia')), " +
+                    "Estado TEXT NOT NULL CHECK (Estado IN ('Pagada','Pendiente')), " +
+                    "FechaPago TEXT, " +
+                    "FechaVencimiento TEXT NOT NULL, " +
+                    "CantCuotas INTEGER DEFAULT 0, " +
+                    "UltDigitosTarj INTEGER DEFAULT 0, " +
+                    "FOREIGN KEY (id_cliente) REFERENCES clientes(id) ON DELETE CASCADE" +
+                    ")"
+        )
+
+        db.execSQL(
+            "CREATE TABLE pago_actividad(" +
+                    "IdPagoActividad INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "id_cliente INTEGER NOT NULL, " +
+                    "IdActividad INTEGER NOT NULL, " +
+                    "FechaPago TEXT NOT NULL, " +
+                    "ModoPago TEXT NOT NULL CHECK (ModoPago IN ('Efectivo','Tarjeta','Transferencia')) DEFAULT 'Efectivo', " +
+                    "Monto REAL NOT NULL, " +
+                    "Estado TEXT NOT NULL CHECK (Estado IN ('Pagada','Pendiente')) DEFAULT 'Pendiente', " +
+                    "FOREIGN KEY (id_cliente) REFERENCES clientes(id) ON DELETE CASCADE, " +
+                    "FOREIGN KEY (IdActividad) REFERENCES actividades(id) ON DELETE CASCADE" +
+                    ")"
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
@@ -54,6 +97,10 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "SportifyClub.db", 
         db.execSQL("DROP TABLE IF EXISTS actividades")
         db.execSQL("DROP TABLE IF EXISTS usuarios")
         db.execSQL("DROP TABLE IF EXISTS roles")
+        db.execSQL("DROP TABLE IF EXISTS socios")
+        db.execSQL("DROP TABLE IF EXISTS nosocios")
+        db.execSQL("DROP TABLE IF EXISTS cuotas")
+        db.execSQL("DROP TABLE IF EXISTS pago_actividad")
         onCreate(db)
     }
 }
